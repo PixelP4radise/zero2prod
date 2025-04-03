cf1ca14eadd06c1d1fe2648944206381e759801b
Guilherme Proenca Maia Araujo <guilhermeproencaaraujo@gmail.com>
2025-04-03 09:56:00 +0100

    changing logic into the library for easier testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,25 @@
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind adress");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let adress = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check",adress))
         .send()
         .await
         .expect("Failed to execute request");
@@ -14,8 +28,3 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind adress");
-
-    let _ = tokio::spawn(server);
-}
