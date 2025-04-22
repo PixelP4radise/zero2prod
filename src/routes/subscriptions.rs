db96ea13830cd0abc5801ff1ac1b31c17d9b3c2c
Guilherme Proenca Maia Araujo <guilhermeproencaaraujo@gmail.com>
2025-04-22 11:40:06 +0100

    adding query info to logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,12 +10,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber",
+        form.email,
+        form.name
+    );
     log::info!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
-INSERT INTO subscriptions(id, email, name, subscribed_at)
-VALUES ($1, $2, $3, $4)
-"#,
+    INSERT INTO subscriptions(id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+    "#,
         Uuid::new_v4(),
         form.email,
         form.name,
